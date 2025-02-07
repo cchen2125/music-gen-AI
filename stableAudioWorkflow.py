19c0767ff6d3d164a9bdcc0f451cc2f268920966
cchen2125 <cchen2125@gmail.com>
2025-02-07 01:25:38 -0500

    Completed stable audio workflow

--- stableAudioWorkflow.py
+++ stableAudioWorkflow.py
@@ -13,13 +13,19 @@ import sys
 import threading
 import time
 
-NUM_DRIVERS = 1
+NUM_DRIVERS = 3
 
 active_drivers = []
 version = 132
 
 for i in range(NUM_DRIVERS):
-    active_drivers.append(uc.Chrome(version_main=version))
+    account = Protection.sterne_names[i].split('@')[0]
+
+    chromeOptions = uc.ChromeOptions()
+    prefs = {"download.default_directory" : f"/Users/clarachen/Documents/Harvard/Radcliffe Research Partners/music-gen-AI/stable_audio/{account}"}
+    chromeOptions.add_experimental_option("prefs",prefs)
+
+    active_drivers.append(uc.Chrome(version_main=version, options=chromeOptions))
 
 
 # Global event to control the spinner
@@ -86,14 +92,68 @@ def login():
 
 
 def create(q=None):
-    return
+    for driver in active_drivers:
+        if q==None:
+            # SINGLE CASE
+            QUERY = input("What kind of song do you want? Include duration, vibe, era, etc: ")
+        else:
+            # BULK CASE
+            QUERY = q
+
+        query_field = driver.find_element(By.XPATH, '/html/body/div/main/div[1]/div/div/div/div/div[1]/div/div[1]/div[2]/textarea')
+        query_field.clear()
+        query_field.send_keys(QUERY)
+
+        random_sleep(1,2)
+
+        duration_min = driver.find_element(By.XPATH, '/html/body/div/main/div[1]/div/div/div/div/div[1]/div/div[2]/div/div[5]/span/span[1]/input')
+        duration_min.send_keys(1)
+
+        random_sleep(1, 2)
+    
+        generate_btn = driver.find_element(By.XPATH, '/html/body/div/main/div[1]/div/div/div/div/div[2]/div[2]/button')
+        generate_btn.click()
+    
+    spinner_thread = start_loading("CREATION (will take up to 60 seconds)")
+    random_sleep(50, 60)
+    stop_loading(spinner_thread, "CREATION FINISHED")
 
 
 def download():
-    return
+    spinner_thread = start_loading("DOWNLOAD")
+
+    for driver in active_drivers:
+
+        latest_song = driver.find_element(By.XPATH, '/html/body/div/main/div[3]/div[2]/div/div[2]/div[2]/div[1]')
+        latest_song.click()
+
+        download_btn = driver.find_element(By.XPATH, '/html/body/div/main/div[3]/div[2]/div/div[2]/div[2]/div[1]/div/div[3]/button[2]')
+        download_btn.click()
+
+        mp3_btn = driver.find_element(By.XPATH, '/html/body/div/div[1]/div/div/div[1]/button')
+        mp3_btn.click()
+
+        random_sleep(1, 2)
+
+        close_btn = driver.find_element(By.XPATH, '/html/body/div/div[1]/div/button')
+        close_btn.click()
+
+    random_sleep(1, 3)
+
+    stop_loading(spinner_thread, "DOWNLOAD FINISHED")
 
 
 def bulk_create_and_download(QUERY_LIST):
+    if len(QUERY_LIST)>10:
+        print("MAX OF 10 SONGS PER MONTH!!")
+        return
+
+    counter = 1
+    for query_key in QUERY_LIST:
+        print(f"STARTING DOWNLOAD NUMBER: {counter} / {len(QUERY_LIST)}")
+        create(QUERY_LIST[query_key])
+        download()
+        counter+=1
     return
 
 
@@ -106,19 +166,22 @@ def main():
             d.close()
         return
 
-    # batch = {'yoga': "Compose a track for a yoga session. The music should align with the calming and focused nature of the activity.",
-    #     'weightlifting': "Compose a track for a weightlifting session. The music should match the intense and dynamic energy of the workout."
-    #     }
+    batch = {
+        "indigenous_ceremony": "Compose a track for an Indigenous ceremony. The music should respect traditional rhythms and cultural significance.",
+        "black_history_month": "Compose a track celebrating Black History Month. The music should reflect pride, resilience, and cultural heritage.",
+        "new_year": "Compose a cheerful, upbeat track for celebrating the new year.",
+        "lunar_new_year": "Compose a cheerful, upbeat track for celebrating lunar new year"
+        }
 
-    # if input("Do you want a batch query? (y/n): ") == "y":
-    #     print("Okay, here is your batch list: ")
-    #     print(batch)
-    #     bulk_create_and_download(batch)
-    #     time.sleep(3)
+    if input("Do you want a batch query? (y/n): ") == "y":
+        print("Okay, here is your batch list: ")
+        print(batch)
+        bulk_create_and_download(batch)
+        time.sleep(3)
 
-    # while input("Do you want another song? (y/n): ") != "n":
-    #     create()
-    #     download()
+    while input("Do you want another song? (y/n): ") != "n":
+        create()
+        download()
 
 
     print("ENDING SESSION")
